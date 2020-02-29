e3d3cbb76555fa716e2b479a9464e6a2654e4e9d
Joseph Burton <josephburton06@gmail.com>
2020-02-29 16:55:14 -0600

    Update Case

--- case_MYSQL_practice.sql
+++ case_MYSQL_practice.sql
@@ -16,6 +16,9 @@ FROM employees;
 -- How many employees were born in each decade?
 
 SELECT
+	-- This Substring is used to take the third digit from the employee's birth
+    -- year, which is the decadel  We can then group by that to get a count of
+    -- employees born in each decade.
 	SUBSTR(birth_date, 3, 1) AS birth_decade
     ,COUNT(*)
 
@@ -55,3 +58,28 @@ LEFT JOIN salaries AS b
 ON a.emp_no = b.emp_no
 
 ORDER BY emp_no, salary DESC;
+
+-- Write a query that returns all employees (emp_no), their department number, 
+-- their start date, their end date, and a new column 'is_current_employee' 
+-- that is a 1 if the employee is still with the company and 0 if not.
+
+SELECT 
+	a.*
+    ,IF(to_date LIKE '9999%', 1, 0) AS is_current_employer
+    
+FROM dept_emp AS a
+
+INNER JOIN 
+-- This subquery is used to create a list of an employee's most recent dept.
+-- By Inner Joining back to the dept_emp table, we eliminate rows that are not
+-- an employee's most recent department.
+	(
+	SELECT 
+		emp_no
+		,MAX(from_date) AS most_rec_dept_date
+	FROM dept_emp
+	GROUP BY emp_no
+	) AS b 
+ON a.emp_no = b.emp_no AND a.from_date = b.most_rec_dept_date
+
+ORDER BY a.emp_no
